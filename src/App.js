408cbb08b69352dfe4e073eb42373d7d6233a250
Colin Rosati <colin.james.rosati@gmail.com>
2019-12-10 11:51:20 +0100

    create two components AppBody and SelectCarModal. Connect redux store to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import AppBody from './components/AppBody'
+import SelectCarModal from './components/ SelectCarModal'
+
+import { connect } from 'react-redux'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Friday Car Insurance</h1>
+        <h5>select your car from database</h5>
       </header>
+     <AppBody/>
+     <SelectCarModal/>
     </div>
   );
 }
 
-export default App;
+export default connect()(App);
--- src/reducers/database-reducer.js
+++ src/reducers/database-reducer.js
@@ -1,4 +1,4 @@
-import { UPDATE_CAR_API_REQUEST, SHOW_CAR_API_REQUEST_ERROR } from '../actions/history-action'
+import { UPDATE_CAR_API_REQUEST, SHOW_CAR_API_REQUEST_ERROR } from '../actions/database-action'
 
 export default function databaseReducer(state = '', { type, payload }) {
     switch (type) {
